486a23b54665680eef46678e9c4cfa7eee6530cd
AbdelhakBrahem <abdelhakbrahem@gmail.com>
2023-04-20 01:01:47 +0100

    first commit

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { MessageComponent } from './signup/message/message.component';
+import { FormComponent } from './signup/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    MessageComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
